2182081551ddbd1fa7a1b99967fdcf7b6109cd22
Josh Terrell <joshterrell805@gmail.com>
2024-06-30 14:08:51 -0400

    Refactor main

--- active-member-discord-bot/src/main/kotlin/regenerativeag/Main.kt
+++ active-member-discord-bot/src/main/kotlin/regenerativeag/Main.kt
@@ -4,23 +4,27 @@ import io.ktor.client.*
 import io.ktor.client.plugins.*
 import kotlin.system.exitProcess
 
-
 fun main(args: Array<String>) {
     val token = System.getenv("DISCORD_API_TOKEN")
     if (token.isNullOrEmpty()) {
         println("Please set the DISCORD_API_TOKEN env var")
-        exitProcess(-1)
     } else {
-        val httpClient = HttpClient {
-            expectSuccess = false
-            install(HttpRequestRetry) {
-                retryOnServerErrors(3)
-                exponentialDelay()
-            }
-        }
-        val discord = Discord(httpClient, token)
-        val database = Database()
-        val bot = ActiveMemberDiscordBot(database, discord)
-        bot.login()
+        run(token)
+    }
+}
+
+private fun run(val discordApiToken: String) {
+    val httpClient = createHttpClient()
+    val discord = Discord(httpClient, discordApiToken)
+    val database = Database()
+    val bot = ActiveMemberDiscordBot(database, discord)
+    bot.login()
+}
+
+private fun createHttpClient() = HttpClient {
+    expectSuccess = false
+    install(HttpRequestRetry) {
+        retryOnServerErrors(3)
+        exponentialDelay()
     }
 }
