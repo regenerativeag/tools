9eacb780dc9d7b197d1e13370b967e2f9b7806bf
Josh Terrell <joshterrell805@gmail.com>
2024-12-03 17:02:12 -0500

    simplify reaction service & fix bug

--- gatekeeper-bot/src/main/kotlin/org/regenagcoop/discord/service/PersistReactionService.kt
+++ gatekeeper-bot/src/main/kotlin/org/regenagcoop/discord/service/PersistReactionService.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import org.regenagcoop.discord.Discord
 import org.regenagcoop.discord.model.Message
-import org.regenagcoop.discord.model.MessageId
 import org.regenagcoop.discord.model.UserId
 import org.regenagcoop.model.ActiveMemberConfig
 import java.time.LocalDate
@@ -13,68 +12,64 @@ import java.time.LocalDate
 class PersistReactionService(
     private val discord: Discord,
     activeMemberConfig: ActiveMemberConfig,
-    today: LocalDate,
+    private var today: LocalDate,
     persistedMessages: List<Message>,
 ) {
     private val logger = KotlinLogging.logger { }
     private val channelId = activeMemberConfig.persistenceConfig.channel
     private val mutex = Mutex()
-    private var yesterdayMessageInfo: MessageInfo? = findMessageInfoFor(today.minusDays(1), persistedMessages)
-    private var todayMessageInfo: MessageInfo? = findMessageInfoFor(today, persistedMessages)
+    private var yesterdayMessage: Message? = findMessageFor(today.minusDays(1), persistedMessages)
+    private var todayMessage: Message? = findMessageFor(today, persistedMessages)
 
     suspend fun persistReaction(date: LocalDate, userId: UserId) {
         mutex.withLock {
-            // TODO fix bug: should be able to add to yesterday's message even if today's message doesn't exist yet
-            if (todayMessageInfo == null) {
-                logger.debug { "Creating new reaction history message for today, $date, starting with userId=$userId" }
-                todayMessageInfo = postNewReactionHistoryMessage(date, userId)
-            } else if (date == todayMessageInfo!!.forDate) {
-                logger.debug { "Adding userId=$userId to today's reaction history message ($date)" }
-                todayMessageInfo = editReactionHistoryMessage(todayMessageInfo!!, date, userId)
-            } else if (date.plusDays(1) == todayMessageInfo!!.forDate) {
-                if (yesterdayMessageInfo == null) {
-                    logger.debug { "Creating new reaction history message for yesterday, $date, starting with userId=$userId" }
-                    yesterdayMessageInfo = postNewReactionHistoryMessage(date, userId)
-                } else {
-                    logger.debug { "Adding userId=$userId to yesterday's reaction history message ($date)" }
-                    if (yesterdayMessageInfo!!.forDate != date) {
-                        throw IllegalStateException("Yesterday's date should be $date, was ${yesterdayMessageInfo!!.forDate}")
-                    }
-                    yesterdayMessageInfo = editReactionHistoryMessage(yesterdayMessageInfo!!, date, userId)
+            val yesterday = today.minusDays(1)
+            val tomorrow = today.plusDays(1)
+            when(date) {
+                today -> {
+                    todayMessage = postOrEditReactionHistoryMessage(date, userId, todayMessage)
                 }
-            } else if (date.minusDays(1) == todayMessageInfo!!.forDate) {
-                logger.debug { "Received reaction for tomorrow. Switching today to yesterday. Creating new reaction history message for today, $date, starting with userId=$userId" }
-                yesterdayMessageInfo = todayMessageInfo
-                todayMessageInfo = postNewReactionHistoryMessage(date, userId)
-            } else {
-                throw IllegalArgumentException("Received reaction for date=$date that was neither today (${todayMessageInfo?.forDate}), yesterday (${yesterdayMessageInfo?.forDate}), nor tomorrow.")
+                yesterday -> {
+                    yesterdayMessage = postOrEditReactionHistoryMessage(date, userId, yesterdayMessage)
+                }
+                tomorrow -> {
+                    logger.debug { "Received reaction for tomorrow. Switching today ($today) to yesterday ($yesterday) and creating a new today ($tomorrow)." }
+                    yesterdayMessage = todayMessage
+                    today = today.plusDays(1)
+                    todayMessage = postNewReactionHistoryMessage(date, userId)
+                }
+                else -> throw IllegalArgumentException("Received reaction for userId=$userId, however reactionDate=$date was neither today ($today), yesterday ($yesterday), nor tomorrow ($tomorrow).")
             }
         }
     }
 
-    private suspend fun postNewReactionHistoryMessage(date: LocalDate, userId: UserId): MessageInfo {
-        val message = discord.rooms.postMessage(createMessage(date, userId), channelId)
-        return MessageInfo(date, message.messageId, message.text)
+    private suspend fun postOrEditReactionHistoryMessage(
+        date: LocalDate,
+        userId: UserId,
+        existingMessageInfo: Message?
+    ) = if (existingMessageInfo == null) {
+        postNewReactionHistoryMessage(date, userId)
+    } else {
+        editReactionHistoryMessage(existingMessageInfo, date, userId)
     }
 
-    private suspend fun editReactionHistoryMessage(messageToEdit: MessageInfo, date: LocalDate, userId: UserId): MessageInfo {
-        val message = discord.rooms.editMessage(channelId, messageToEdit.messageId, "${messageToEdit.currentText}, $userId")
-        return MessageInfo(date, message.messageId, message.text)
+    private suspend fun postNewReactionHistoryMessage(date: LocalDate, userId: UserId): Message {
+        logger.debug { "Creating new reaction history message for $date, starting with userId=$userId" }
+        return discord.rooms.postMessage(createMessage(date, userId), channelId)
     }
 
-    private fun findMessageInfoFor(date: LocalDate, persistedHistoryMessages: List<Message>): MessageInfo? {
+    private suspend fun editReactionHistoryMessage(messageToEdit: Message, date: LocalDate, userId: UserId): Message {
+        logger.debug { "Adding userId=$userId to $date's reaction history message" }
+        val newText = "${messageToEdit.text}, $userId"
+        return discord.rooms.editMessage(channelId, messageToEdit.messageId, newText)
+    }
+
+    private fun findMessageFor(date: LocalDate, persistedHistoryMessages: List<Message>): Message? {
         val prefix = "Users who reacted on $date:"
-        val message = persistedHistoryMessages.singleOrNull { it.text.startsWith(prefix) }
-        return message?.let {
-            MessageInfo(date, it.messageId, it.text)
-        }
+        return persistedHistoryMessages.singleOrNull { it.text.startsWith(prefix) }
     }
 
     private fun createMessage(date: LocalDate, firstUser: UserId): String {
         return "Users who reacted on $date: $firstUser"
-    }
-
-    private companion object {
-        data class MessageInfo(val forDate: LocalDate, val messageId: MessageId, val currentText: String)
     }
 }
