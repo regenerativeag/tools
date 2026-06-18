e97b1f9d17ceb7a028d2282942ae92b749a835a2
joshterrell805@gmail.com <joshterrell805@gmail.com>
2026-06-18 13:17:03 +0000

    refactor: command names (post & edit)

--- gatekeeper-bot/src/main/kotlin/org/regenagcoop/model/config/SlashCommandConfig.kt
+++ gatekeeper-bot/src/main/kotlin/org/regenagcoop/model/config/SlashCommandConfig.kt
@@ -1,8 +1,6 @@
 package org.regenagcoop.model.config
 
 data class SlashCommandConfig(
-    /** Command users run as `/post` by default */
-    val postCommand: String = "post",
-    /** Command users run as `/edit` by default */
-    val editCommand: String = "edit",
+    val postCommand: String,
+    val editCommand: String,
 )
